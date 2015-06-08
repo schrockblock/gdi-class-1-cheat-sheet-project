3c0613eaba66bc1cd28b02a830ed82959e9ac731
Elliot <elliot.schrock@gmail.com>
2015-06-08 19:27:11 -0400

    Initial Commit

---
+++ ElliotTriesToCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ElliotTriesToCode
+//
+//  Created by Elliot Schrock on 6/8/15.
+//  Copyright (c) 2015 Elliot Schrock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ElliotTriesToCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ElliotTriesToCode
+//
+//  Created by Elliot Schrock on 6/8/15.
+//  Copyright (c) 2015 Elliot Schrock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
